2676094fe0b00e8cd8a5085abba15ee4c3810910
HannaYakubouskaya <161929265+HannaYakubouskaya@users.noreply.github.com>
2025-12-01 16:37:40 +0100

    Initial commit

--- utils/reminders.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from aiogram.dispatcher import FSMContext
-from config import bot
-
-async def reminder_timer(user_id, state: FSMContext):
-    await asyncio.sleep(300)  # 5 мин
-    current_state = await state.get_state()
-    if current_state:
-        await bot.send_message(user_id, "Мы ждём твоего ответа! Продолжи запись. ⏰")
