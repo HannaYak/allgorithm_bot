31558fcab120e7a322600de743dfcc5ed14b6383
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-12-05 00:28:24 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
 
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-  throw new Error("DATABASE_URL is required to run drizzle commands");
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing");
 }
 
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql", // <-- Поменяйте 'mysql' на это
+  driver: "pg", // <--- ВОТ ЗДЕСЬ БЫЛА ОШИБКА, НУЖНО ИМЕННО "pg"
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL,
   },
 });
