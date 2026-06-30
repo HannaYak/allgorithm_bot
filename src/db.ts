a1190325431ede36d8660a560c37d04bf77cffe7
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2026-06-30 13:15:31 +0200

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,6 @@
-// src/db.ts
-// Вместо импорта из './index'
-import { db } from './db.js';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from '../drizzle/schema';
+import * as schema from '../drizzle/schema.js';
 import 'dotenv/config';
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is missing');
